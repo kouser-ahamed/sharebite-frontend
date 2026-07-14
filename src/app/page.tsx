1fdbefaf0170863f115ecfeab0f44045a8c4ce30
Kouser Ahamed <kouserahamed.cse.diu@gmail.com>
2026-07-14 21:08:33 +0600

    feat: add LatestFoodsSection component to display recently shared foods

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import LatestFoodsSection from "@/components/all-foods/LatestFoodsSection";
 import HeroSection from "@/components/home/HeroSection";
 import Image from "next/image";
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <div>
       <HeroSection />
+      <LatestFoodsSection />
     </div>
   );
 }

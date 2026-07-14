5e1c4da18d8c81479453c59c6a1b33eff45b8af3
Kouser Ahamed <kouserahamed.cse.diu@gmail.com>
2026-07-15 00:17:37 +0600

    feat: add ExpiringSoonSection component to display foods nearing expiration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,14 @@ import TestimonialsSection from "@/components/home/TestimonialsSection";
 import CommunityImpactStatistics from "@/components/home/CommunityImpactStatistics";
 import VolunteerSection from "@/components/home/VolunteerSection";
 import FAQSection from "@/components/home/FAQSection";
+import ExpiringSoonSection from "@/components/home/ExpiringSoonSection";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
       <LatestFoodsSection />
+      <ExpiringSoonSection />
       <HowShareBiteWorks />
       <TestimonialsSection />
       <CommunityImpactStatistics />

9ef9cacf66d4dbddf19d15647e774a36c5b28f3e
Kouser Ahamed <kouserahamed.cse.diu@gmail.com>
2026-07-14 19:38:15 +0600

    feat: add HeroSection component with image slider and content for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import HeroSection from "@/components/home/HeroSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      Home Page
+      <HeroSection />
     </div>
   );
 }
